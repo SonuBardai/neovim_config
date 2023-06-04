43c4efed7a98ae4b70ac7564f148a9a12b002466
SonuBardai <sonubardai9@gmail.com>
2023-06-04 18:13:54 +0530

    Harpoon, LSP updates

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,9 +1,16 @@
+require("harpoon").setup({ 
+  tabline = true,
+})
+
+vim.cmd('highlight! HarpoonActive guibg=NONE guifg=white')
+vim.cmd('highlight! HarpoonNumberActive guibg=NONE guifg=white')
+
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 -- key maps to use harpoon
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<leader>hm", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>ah", mark.add_file, {desc="[A]dd to [H]arpoon"})
+vim.keymap.set("n", "<leader>hm", ui.toggle_quick_menu, {desc="[H]arpoon [M]enu"})
 
 -- key maps to navigate between files added to harpoon
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
